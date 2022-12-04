2b233790a6a7bd357bd8bef956722f77cd724116
joydeep-git <joydeep.accounts@tuta.io>
2022-12-04 17:29:14 +0530

    React Basic Files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import ToDoList from "./components/ToDoList";
+import AddToDo from "./components/AddToDo";
+import DeleteToDo from "./components/DeleteToDo";
+function App() {
+  return (
+    <>
+    <AddToDo/>
+    </>
+  );
+}
+export default App;

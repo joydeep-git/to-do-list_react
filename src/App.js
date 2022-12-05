b679675de89d7de377c8cdbb35d87622d716b4e7
joydeep-git <joydeep.accounts@tuta.io>
2022-12-05 21:47:09 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ToDoList from "./components/ToDoList";
 import AddToDo from "./components/AddToDo";
+import "./Images/react.svg";
 import { useState } from "react";
 function App() {
   const [toDos, setToDos] = useState([
@@ -11,7 +12,7 @@ function App() {
       fontSize: "15px",
       textAlign: "center",
       fontFamily: "poppins",
-    }}>Created by Joydeep with ❤ & React.js </p>
+    }}>Created by Joydeep with ❤ & <img src="" alt="React.JS" /> </p>
     <h1 style={{
       textAlign:"center",
       fontSize:"1.5rem",
